5f1004d393be126de0700bb8605f966946451529
Ugorji Nwoke <ugorji@gmail.com>
2015-08-02 23:02:24 -0400

    codec: update files to say MIT (not BSD-style) LICENSE, in line with the bundled license.
    Fixes #87

--- codec/prebuild.sh
+++ codec/prebuild.sh
@@ -54,7 +54,7 @@ _build() {
 
     cat > gen.generated.go <<EOF
 // Copyright (c) 2012-2015 Ugorji Nwoke. All rights reserved.
-// Use of this source code is governed by a BSD-style license found in the LICENSE file.
+// Use of this source code is governed by a MIT license found in the LICENSE file.
 
 package codec
 
