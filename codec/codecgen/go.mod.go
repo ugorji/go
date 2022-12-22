6f924ee409c2aba5caa0a11a2d469cb5dd7dd11f
Ugorji Nwoke <ugorji@gmail.com>
2022-12-22 03:24:48 -0500

    codecgen: separate blank import for codec package to be ignored during build

--- codec/codecgen/go.mod.go
+++ codec/codecgen/go.mod.go
@@ -1,6 +1,9 @@
 // Copyright (c) 2012-2020 Ugorji Nwoke. All rights reserved.
 // Use of this source code is governed by a MIT license found in the LICENSE file.
 
+//go:build go.mod.ignore
+// +build go.mod.ignore
+
 package main
 
-const genCodecPath = "github.com/ugorji/go/codec"
+import _ "github.com/ugorji/go/codec" // ensures the go.mod keeps this dependency
