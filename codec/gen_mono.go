c719da2e5596740a2b629e5390deafb97e466a25
Ugorji Nwoke <ugorji@users.noreply.github.com>
2025-03-25 19:41:48 -0400

    codec: fix issue monomorphizing multiple formats and clean up comments

--- codec/gen_mono.go
+++ codec/gen_mono.go
@@ -17,7 +17,6 @@ import (
 )
 
 // ----
-// MARKER 2025
 
 // - [fn:monoBase] for each file of fast-path.generated.go, encode.go, decode.go:
 //   - ignore init() and imports (go imports will fix it later - or we just grab all imports and let go-imports fix later)
@@ -37,7 +36,7 @@ const genMonoParserMode = parser.AllErrors | parser.SkipObjectResolution
 //
 // However, for types bound to encWriter or decReader, which are shared across formats,
 // there's no place to put them without duplication.
-//
+
 // Consequently, since there is only one function ie decByteSlice, we handle it manually.
 // This method is in the type helperDecReader.
 var genMonoSpecialFieldTypes = []string{"helperDecReader"}
@@ -45,8 +44,10 @@ var genMonoSpecialFieldTypes = []string{"helperDecReader"}
 // These functions should take the address of first param when monomorphized
 var genMonoSpecialFunc4Addr = []string{"decByteSlice"}
 
+var genMonoImportsToSkip = []string{`"errors"`, `"fmt"`, `"net/rpc"`}
+
 var genMonoRefImportsVia_ = [][2]string{
-	{"errors", "New"},
+	// {"errors", "New"},
 }
 
 type genMono struct {
@@ -66,6 +67,7 @@ func (x *genMono) reset() {
 	clear(x.typParam)
 	clear(x.imports)
 	clear(x.importSpecs)
+	x.importSpecs = x.importSpecs[:0]
 }
 
 func (m *genMono) do(h Handle) {
@@ -74,26 +76,18 @@ func (m *genMono) do(h Handle) {
 	b, fset := m.base(true)
 	r1 := m.handle(h, b, fset)
 	m.trFile(r1, h, true)
-	// clear(m.typParam)
+	clear(m.typParam)
 
 	b, fset = m.base(false)
 	r2 := m.handle(h, b, fset)
 	m.trFile(r2, h, false)
-	// clear(m.typParam)
+	clear(m.typParam)
 
 	r0 := &ast.File{Name: &ast.Ident{Name: "codec"}}
 	r0.Decls = append(r0.Decls, &ast.GenDecl{Tok: token.IMPORT, Specs: m.importSpecs})
-	_vd := &ast.GenDecl{Tok: token.VAR}
-	for _, v := range genMonoRefImportsVia_ {
-		_vs := new(ast.ValueSpec)
-		_vs.Names = append(_vs.Names, &ast.Ident{Name: "_"})
-		_vs.Values = append(_vs.Values, &ast.SelectorExpr{
-			X:   &ast.Ident{Name: v[0]},
-			Sel: &ast.Ident{Name: v[1]},
-		})
-		_vd.Specs = append(_vd.Specs, _vs)
+	if _vd := genMonoRefImportsVia_Decl(); _vd != nil {
+		r0.Decls = append(r0.Decls, _vd)
 	}
-	r0.Decls = append(r0.Decls, _vd)
 	r0.Decls = append(r0.Decls, r1.Decls...)
 	r0.Decls = append(r0.Decls, r2.Decls...)
 
@@ -129,7 +123,6 @@ func (x *genMono) base(isbytes bool) (r *ast.File, fset *token.FileSet) {
 		Name: &ast.Ident{Name: "codec"},
 	}
 	fnames := []string{"encode.go", "decode.go", "fast-path.generated.go"}
-	// fnames = []string{"encode.go", "decode.go", "fast-path.not.go"} // MARKER 2025 - remove this
 	for _, fname = range fnames {
 		fsrc := x.file(fname)
 		f, err := parser.ParseFile(fset, fname, fsrc, genMonoParserMode)
@@ -201,6 +194,9 @@ func (x *genMono) merge(dst, src *ast.File) {
 			} else if n.Tok == token.IMPORT {
 				for _, v := range n.Specs {
 					nn := v.(*ast.ImportSpec)
+					if slices.Contains(genMonoImportsToSkip, nn.Path.Value) {
+						continue
+					}
 					if _, ok := x.imports[nn.Path.Value]; !ok {
 						x.importSpecs = append(x.importSpecs, v)
 						x.imports[nn.Path.Value] = struct{}{}
@@ -312,7 +308,7 @@ func (x *genMono) trArray(n *ast.ArrayType, tp *ast.Field, h Handle, isbytes boo
 	// type fastpathEs[T encDriver] [56]fastpathE[T]
 	// p := tp.Names[0].Name
 	switch elt := n.Elt.(type) {
-	// case *ast.InterfaceType: // MARKER 2025
+	// case *ast.InterfaceType:
 	case *ast.IndexExpr:
 		if elt.Index.(*ast.Ident).Name == "T" { // generic
 			n.Elt = ast.NewIdent(elt.X.(*ast.Ident).Name + hnameUp + sfx)
@@ -348,7 +344,6 @@ func (x *genMono) trMethodBody(r *ast.BlockStmt, tp *ast.Field, h Handle, isbyte
 					fnUp()
 				}
 			}
-			// special case: if decByteSlice function, take addr of first parameter // MARKER 2025
 			if n4, ok4 := n.Fun.(*ast.Ident); ok4 && slices.Contains(genMonoSpecialFunc4Addr, n4.Name) {
 				n.Args[0] = &ast.UnaryExpr{Op: token.AND, X: n.Args[0].(*ast.SelectorExpr)}
 			}
@@ -420,9 +415,6 @@ func (x *genMono) trField(f *ast.Field, tpt *ast.Field, h Handle, isbytes, isRec
 	}
 
 	tp = x.updateIdentForT(pn, h, tpt, isbytes, true)
-	// if isRecv {
-	// 	fmt.Printf(">>>> pn: %s\n", pn)
-	// }
 	return
 }
 
@@ -434,10 +426,8 @@ func (x *genMono) updateIdentForT(pn *ast.Ident, h Handle, tp *ast.Field, isbyte
 		return
 	}
 
-	// fmt.Printf(">>>> tp: %v\n", tp)
 	if pn.Name != "T" && lookupTP {
 		tp = x.typParam[pn.Name]
-		// fmt.Printf(">>>> tp updated: %v\n", tp)
 	}
 
 	paramtyp := tp.Type.(*ast.Ident).Name
@@ -507,6 +497,23 @@ func genMonoCopy(src *ast.File) (dst *ast.File) {
 	return
 }
 
+func genMonoRefImportsVia_Decl() (_vd *ast.GenDecl) {
+	if len(genMonoRefImportsVia_) == 0 {
+		return
+	}
+	_vd = &ast.GenDecl{Tok: token.VAR}
+	for _, v := range genMonoRefImportsVia_ {
+		_vs := new(ast.ValueSpec)
+		_vs.Names = append(_vs.Names, &ast.Ident{Name: "_"})
+		_vs.Values = append(_vs.Values, &ast.SelectorExpr{
+			X:   &ast.Ident{Name: v[0]},
+			Sel: &ast.Ident{Name: v[1]},
+		})
+		_vd.Specs = append(_vd.Specs, _vs)
+	}
+	return
+}
+
 func GenMonoAll() {
 	hdls := []Handle{
 		(*SimpleHandle)(nil),
@@ -515,7 +522,6 @@ func GenMonoAll() {
 		(*BincHandle)(nil),
 		(*MsgpackHandle)(nil),
 	}
-	// hdls = []Handle{(*MsgpackHandle)(nil)} // MARKER 2025 remove
 	var m genMono
 	m.init()
 	for _, v := range hdls {
@@ -523,505 +529,3 @@ func GenMonoAll() {
 		runtime.GC()
 	}
 }
-
-/*
-const genMonoBaseStrBytes = `
-package codec
-
-type helperDecReaderBytes struct{}
-
-func (helperDecReaderBytes) decByteSlice(r *bytesDecReader, clen, maxInitLen int, bs []byte) (bsOut []byte) {
-	if clen <= 0 {
-		bsOut = zeroByteSlice
-	} else if cap(bs) >= clen {
-		bsOut = bs[:clen]
-		r.readb(bsOut)
-	} else {
-		var len2 int
-		for len2 < clen {
-			len3 := decInferLen(clen-len2, maxInitLen, 1)
-			bs3 := bsOut
-			bsOut = make([]byte, len2+len3)
-			copy(bsOut, bs3)
-			r.readb(bsOut[len2:])
-			len2 += len3
-		}
-	}
-	return
-}
-`
-
-const genMonoBaseStrIO = `
-package codec
-
-type helperDecReaderIO struct{}
-
-func (helperDecReaderIO) decByteSlice(r *ioDecReader, clen, maxInitLen int, bs []byte) (bsOut []byte) {
-	if clen <= 0 {
-		bsOut = zeroByteSlice
-	} else if cap(bs) >= clen {
-		bsOut = bs[:clen]
-		r.readb(bsOut)
-	} else {
-		var len2 int
-		for len2 < clen {
-			len3 := decInferLen(clen-len2, maxInitLen, 1)
-			bs3 := bsOut
-			bsOut = make([]byte, len2+len3)
-			copy(bsOut, bs3)
-			r.readb(bsOut[len2:])
-			len2 += len3
-		}
-	}
-	return
-}
-`
-
-func (x *genMono) transform(r *ast.File, h Handle, isbytes bool) {
-	var sfx string
-	if isbytes {
-		sfx = "Bytes"
-	} else {
-		sfx = "IO"
-	}
-	up := func(s string) string {
-		return strings.ToUpper(s[:1]) + s[1:]
-	}
-	hname := h.Name()
-
-	// Type parameters only exist in
-	//   - function/method declarations (function, method)
-	//     - signature
-	//     - body
-	//   - type declarations (struct, array)
-	//     - signature
-	//     - body
-	//   - type instantiations (N/A as moved into init.go)
-	//   - type constraints (N/A as moved into init.go)
-	//
-	// Consequently, we only need to work on func and type declarations
-	// and things that flow down from there.
-	//
-	// We only want to work on Field values which could have TypeParams.
-
-	typeParams := make(map[*ast.FieldList]struct{})
-	var typParam *ast.Field
-
-	var stack []ast.Node
-	pop := func() {
-		stack = stack[:len(stack)-1]
-	}
-	push := func(n ast.Node) {
-		stack = append(stack, n)
-	}
-	// last := func(n depth) ast.Node {
-	// 	pos := len(stack) - 1 - n
-	// 	if pos >= 0 {
-	// 		return stack[pos]
-	// 	}
-	// 	return nil
-	// }
-	locate := func(f func(n ast.Node) bool) ast.Node {
-		for i := len(stack) - 1; i >= 0; i-- {
-			if n := stack[i]; f(n) {
-				return n
-			}
-		}
-		return nil
-	}
-
-	locateTypeSpec := func() *ast.TypeSpec {
-		fp := func(node ast.Node) bool {
-			_, ok := node.(*ast.TypeSpec)
-			return ok
-		}
-		v := locate(fp)
-		if v != nil {
-			return v.(*ast.TypeSpec)
-		}
-		return nil
-	}
-
-	// if type parameter is encDriver, use <name><handle><IO|Bytes>
-	// e.g. if type is encoder[T], do encoderJsonBytes --> jsonEncDriverBytes
-	fnNameViaType := func(nn *ast.Ident) {
-		paramName := typParam.Names[0].Name
-		paramType := typParam.Type.(*ast.Ident).Name
-		if nn.Name == "" || nn.Name == paramName {
-			switch paramType {
-			case "encDriver", "decDriver":
-				nn.Name = hname + up(paramType) + sfx
-			case "encWriter":
-				if isbytes {
-					nn.Name = "bytesEncAppender"
-				} else {
-					nn.Name = "bufioEncWriter"
-				}
-			case "decReader":
-				if isbytes {
-					nn.Name = "bytesDecReader"
-				} else {
-					nn.Name = "ioDecReader"
-				}
-			}
-			return
-		}
-
-		switch paramType {
-		case "encDriver", "decDriver":
-			// encoder or decoder has a field of encDriver / decDriver
-			nn.Name += up(hname) + sfx
-		case "encWriter", "decReader":
-			// already en encDriver or decDriver
-			nn.Name += sfx
-		}
-	}
-
-	fn := func(node ast.Node) bool {
-		if node == nil {
-			pop()
-			return false
-		}
-		push(node)
-
-		typParam = nil
-		switch n := node.(type) {
-		case *ast.FuncDecl:
-			// if !genMonoTypeParamsOk(n.Type.TypeParams) {
-			// 	return false
-			// }
-			// typeParams[n.Type.TypeParams] = struct{}{}
-			// always methods. change method's receiver type only
-
-			var rtyp string
-			switch nn := n.Recv.List[0].Type.(type) {
-			case *ast.IndexExpr:
-				if nn2, ok := nn.X.(*ast.Ident); ok {
-					rtyp = nn2.Name
-				}
-			case *ast.StarExpr:
-				if nn2, ok := nn.X.(*ast.IndexExpr); ok {
-					if nn3, ok := nn2.X.(*ast.Ident); ok {
-						rtyp = nn3.Name
-					}
-				}
-			}
-			if rtyp != "" {
-				typParam = x.typParam[rtyp]
-			}
-			if typParam == nil {
-				return false
-			}
-			// typParam = n.Type.TypeParams.List[0]
-			pnew := ast.NewIdent(rtyp)
-			n.Recv.List[0].Type = pnew
-			fnNameViaType(pnew)
-		case *ast.FuncType:
-			// update the params and results, using type parameters
-			typeParams[n.TypeParams] = struct{}{}
-			// if n.TypeParams == nil {
-			// 	return false
-			// }
-			// field := n.TypeParams.List[0].Type.(*ast.Ident)
-			// for _, v0 := range []*ast.FieldList{n.Params, n.Results} {
-			// 	if v0 == nil {
-			// 		continue
-			// 	}
-			// 	// fmt.Printf("v0: %v, len(v0.List): %v\n", v0, v0)
-			// 	for _, v1 := range v0.List {
-			// 		// fmt.Printf("v1: %v, len(v1.Names): %v\n", v1, len(v1.Names))
-			// 		for _, v2 := range v1.Names {
-			// 			fnNameViaType(v2, field.Name)
-			// 		}
-			// 	}
-			// }
-		case *ast.FieldList:
-			// skip all type params
-			if _, ok := typeParams[n]; ok {
-				return false
-			}
-		case *ast.Field:
-			// type x[T encDriver] struct { w T }
-			if n.Type == nil {
-				return false
-			}
-			p := locateTypeSpec()
-			if p == nil {
-				return false
-			}
-
-			typParam = p.TypeParams.List[0]
-			paramName := typParam.Names[0].Name
-			// if any generic field, handle it using p's TypeParams
-			switch nt := n.Type.(type) {
-			case *ast.Ident:
-				if nt.Name == paramName {
-					fnNameViaType(nt)
-				}
-			case *ast.IndexExpr:
-				if nt.Index.(*ast.Ident).Name == paramName {
-					pnew := ast.NewIdent(nt.X.(*ast.Ident).Name)
-					n.Type = pnew
-					fnNameViaType(pnew)
-				}
-			case *ast.StarExpr:
-				switch ni := nt.X.(type) {
-				case *ast.IndexExpr:
-					if ni.Index.(*ast.Ident).Name == paramName {
-						pnew := ast.NewIdent(ni.X.(*ast.Ident).Name)
-						nt.X = pnew
-						fnNameViaType(pnew)
-					}
-				}
-			}
-		case *ast.TypeSpec:
-			// type x[T encDriver] struct { ... }
-			if !genMonoTypeParamsOk(n.TypeParams) {
-				return false
-			}
-			typeParams[n.TypeParams] = struct{}{}
-			typParam = n.TypeParams.List[0]
-			fnNameViaType(n.Name)
-		case *ast.ValueSpec:
-			// var X encoder[T]
-			p := locateTypeSpec()
-			if p == nil {
-				return false
-			}
-			if !genMonoTypeParamsOk(p.TypeParams) {
-				return false
-			}
-			typParam = p.TypeParams.List[0]
-			// MARKER 2025 - need to set the type of the variable (not the name)
-			// param := p.TypeParams.List[0].Type.(*ast.Ident)
-			// fnNameViaType(n.Name, n.TypeParams.List[0].Type.(*ast.Ident).Name)
-		case *ast.ArrayType:
-			// type fastpathEs[T encDriver] [0]fastpathE[T]
-			p := locateTypeSpec()
-			if p == nil {
-				return false
-			}
-			typParam = p.TypeParams.List[0]
-			paramName := typParam.Names[0].Name
-			switch elt := n.Elt.(type) {
-			case *ast.IndexExpr:
-				if elt.Index.(*ast.Ident).Name == paramName { // generic
-					pnew := ast.NewIdent(elt.X.(*ast.Ident).Name)
-					n.Elt = pnew
-					fnNameViaType(pnew)
-				}
-			case *ast.InterfaceType: // MARKER 2025
-			default:
-			}
-		}
-		return true
-	}
-	ast.Inspect(r, fn)
-
-	// set type params to nil, and Pos to NoPos
-	fn = func(node ast.Node) bool {
-		switch n := node.(type) {
-		// case *ast.FuncDecl:
-		// 	n.Type.TypeParams = nil
-		case *ast.FuncType:
-			if genMonoTypeParamsOk(n.TypeParams) {
-				n.TypeParams = nil
-			}
-		case *ast.TypeSpec: // for type ...
-			if genMonoTypeParamsOk(n.TypeParams) {
-				n.TypeParams = nil
-			}
-		}
-		return true
-	}
-	ast.Inspect(r, fn)
-
-}
-
-func (x *genMono) trMethodBody(r *ast.BlockStmt, tp *ast.Field, h Handle, isbytes bool) {
-	// sfx, writer, reader, hnameUp := genMonoIsBytesVals(h.Name(), isbytes)
-	// find the parent node for an indexExpr, and set the value back in there
-	// var ns genMonoNodeStack
-	fn = func(pnode ast.Node) bool {
-		// if node == nil {
-		// 	ns.pop()
-		// 	return false
-		// }
-		var pn *ast.Ident
-		switch n := pnode.(type) {
-		// case *ast.SelectorExpr:
-		case *ast.IndexExpr:
-			if nn2, ok := nn.X.(*ast.Ident); ok {
-				pn = ast.NewIdent(nn2.Name)
-				nlast := ns.peek()
-				switch nn3 := nlast.(type) {
-				case *ast.StarExpr:
-					f.Type = pn
-				}
-			}
-		}
-		if pn != nil {
-			x.updateIdentForT(pn, h, tp, false)
-		}
-			// ns.push(node) // marker 2025 - should this be here?
-		return true
-	}
-	ast.Inspect(r, fn)
-}
-
-func (x *genMono) trField(f *ast.Field, tpt *ast.Field, h Handle, isbytes, isRecv bool) (tp *ast.Field) {
-	// sfx, writer, reader, hnameUp := genMonoIsBytesVals(h.Name(), isbytes)
-	var pn *ast.Ident
-	fnIdX := func(node2 ast.Node) bool {
-		var pnok bool
-		pn = nil
-		if n2, ok := node2.(*ast.IndexExpr); ok {
-			n9, ok9 := n2.Index.(*ast.Index)
-			n3, ok := n2.X.(*ast.Ident)
-			if ok && ok9 {
-				pn, pnok = ast.NewIdent(n3.Name), true
-			}
-		}
-		return pnok
-	}
-	switch nn := f.Type.(type) {
-	case *ast.IndexExpr:
-		if fnIdX(nn) {
-			f.Type = pn
-		}
-		if nn2, ok := nn.X.(*ast.Ident); ok {
-			pn = ast.NewIdent(nn2.Name)
-			f.Type = pn
-		}
-	case *ast.StarExpr:
-		if nn2, ok := nn.X.(*ast.IndexExpr); ok {
-			if nn3, ok := nn2.X.(*ast.Ident); ok {
-				pn = ast.NewIdent(nn3.Name)
-				nn.X = pn
-			}
-		}
-	case *ast.FuncType:
-		x.trMethodSignNonRecv(nn.Params, tpt, h, isbytes)
-		x.trMethodSignNonRecv(nn.Results, tpt, h, isbytes)
-		return
-	case *ast.ArrayType:
-		x.trArray(nn, tpt, h, isbytes)
-		return
-	case *ast.Ident:
-		if isRecv || nn.Name != "T" {
-			return
-		}
-		pn = nn
-	}
-	if pn == nil {
-		return
-	}
-
-	x.updateIdentForT(pn, h, tpt, isbytes, true)
-	// // handle special ones e.g. helperDecReader et al
-	// if slices.Contains(genMonoSpecialFieldTypes, pn.Name) {
-	// 	pn.Name += sfx
-	// 	return
-	// }
-
-	// if pn.Name == "T" {
-	// 	tp = tpt
-	// } else {
-	// 	tp = x.typParam[pn.Name]
-	// }
-
-	// paramtyp := tp.Type.(*ast.Ident).Name
-	// if pn.Name == "T" {
-	// 	switch paramtyp {
-	// 	case "encDriver", "decDriver":
-	// 		pn.Name = h.Name() + genMonoTitleCase(paramtyp) + sfx
-	// 	case "encWriter":
-	// 		pn.Name = writer
-	// 	case "decReader":
-	// 		pn.Name = reader
-	// 	}
-	// } else {
-	// 	switch paramtyp {
-	// 	case "encDriver", "decDriver":
-	// 		pn.Name += hnameUp + sfx
-	// 	case "encWriter", "decReader":
-	// 		pn.Name += sfx
-	// 	}
-	// }
-	return
-}
-
-const genMonoBaseStrBytes = `
-package codec
-`
-
-const genMonoBaseStrIO = `
-package codec
-`
-
-func (x *genMono) base(isbytes bool) (r *ast.File, fset *token.FileSet) {
-	fsrc := genMonoBaseStrIO
-	fname := "base.io.mono.go"
-	if isbytes {
-		fsrc = genMonoBaseStrBytes
-		fname = "base.bytes.mono.go"
-	}
-
-	fset = token.NewFileSet()
-	r, err := parser.ParseFile(fset, fname, fsrc, genMonoParserMode)
-	halt.onerror(err)
-	r = &ast.File{
-		Name: &ast.Ident{Name: "codec"},
-	}
-	fnames := []string{"encode.go", "decode.go", "fast-path.generated.go"}
-	fnames = []string{"encode.go", "decode.go", "fast-path.not.go"} // MARKER 2025 - remove this
-	for _, fname = range fnames {
-		fsrc := x.file(fname)
-		f, err := parser.ParseFile(fset, fname, fsrc, genMonoParserMode)
-		halt.onerror(err)
-		x.merge(r, f)
-	}
-	return
-    }
-
-type genMonoNodeStack struct {
-	n []ast.Node
-}
-
-func (x *genMonoNodeStack) pop() {
-	x.n = x.n[:len(x.n)-1]
-}
-func (x *genMonoNodeStack) push(n ast.Node) {
-	x.n = append(x.n, n)
-}
-func (x *genMonoNodeStack) peek() (n ast.Node) {
-	if pos := len(x.n) - 1; pos >= 0 {
-		n = x.n[pos]
-	}
-	return
-}
-
-// ----
-
-			} else if n.Tok == token.IMPORT {
-				for _, v := range n.Specs {
-					nn := v.(*ast.ImportSpec)
-					if _, ok := x.imports[nn.Path.Value]; !ok {
-						// dst.Decls = append(dst.Decls, n)
-						// dd := &ast.GenDecl{Tok: n.Tok, Specs: []ast.Spec{v}}
-						// dst.Decls = slices.Insert[[]ast.Decl, ast.Decl](dst.Decls, len(x.imports), dd)
-
-						// ispec := &ast.ImportSpec{
-						// 	Path: &ast.BasicLit{
-						// 		Kind: token.STRING,
-						// 		Value: nn.Path.Value
-						x.importSpecs = append(x.importSpecs, v)
-						x.imports[nn.Path.Value] = struct{}{}
-					}
-				}
-			}
-
-
-// ----
-
-*/
