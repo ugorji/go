b73c52dd9777f275d3928c4ec255dbb5769217af
Ugorji Nwoke <ugorji@users.noreply.github.com>
2025-04-25 04:38:00 -0400

    codec: all benchmark files only built on go1.24

--- codec/z_all_bench_test.go
+++ codec/z_all_bench_test.go
@@ -1,4 +1,4 @@
-//go:build alltests
+//go:build alltests && go1.24
 
 // Copyright (c) 2012-2020 Ugorji Nwoke. All rights reserved.
 // Use of this source code is governed by a MIT license found in the LICENSE file.
