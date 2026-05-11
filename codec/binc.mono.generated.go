cdf3ff40b2946af4830f8a62b8799c4abc9abda9
Ugorji Nwoke <ugorji@users.noreply.github.com>
2026-05-11 00:16:01 -0400

    codec: re-generate files, to remove extra unnecessary space in go:build lines

--- codec/binc.mono.generated.go
+++ codec/binc.mono.generated.go
@@ -1,4 +1,4 @@
-//go:build !notmono && !codec.notmono 
+//go:build !notmono && !codec.notmono
 
 // Copyright (c) 2012-2020 Ugorji Nwoke. All rights reserved.
 // Use of this source code is governed by a MIT license found in the LICENSE file.
